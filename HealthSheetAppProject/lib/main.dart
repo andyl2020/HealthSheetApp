b94f09e43e954deade34e55b307246d6d39ed2d7
Andy L <andyl2020@yahoo.ca>
2021-05-31 00:58:13 -0700

    re-routed to fitness app as homescreen

--- HealthSheetAppProject/lib/main.dart
+++ HealthSheetAppProject/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 import 'package:best_flutter_ui_templates/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'navigation_home_screen.dart';
+import 'package:best_flutter_ui_templates/fitness_app/fitness_app_home_screen.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 void main() async {
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         textTheme: AppTheme.textTheme,
         platform: TargetPlatform.iOS,
       ),
-      home: NavigationHomeScreen(),
+      home: FitnessAppHomeScreen(),
     );
   }
 }
