48f00fef759e3d73b61c6a742299981a11cd50b2
Andy L <andyl2020@yahoo.ca>
2021-06-01 16:53:19 -0700

    fixed apply button (on page scroll view now)

--- HealthSheetAppProject/lib/hotel_booking/filters_screen.dart
+++ HealthSheetAppProject/lib/hotel_booking/filters_screen.dart
@@ -57,50 +57,50 @@ class _FiltersScreenState extends State<FiltersScreen> {
                       height: 1,
                     ),
                     healthAppCheckBoxGroup(),
-                  ],
-                ),
-              ),
-            ),
-            const Divider(
-              height: 1,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 16, right: 16, bottom: 16, top: 8),
-              child: Container(
-                height: 48,
-                decoration: BoxDecoration(
-                  color: HotelAppTheme.buildLightTheme().primaryColor,
-                  borderRadius: const BorderRadius.all(Radius.circular(24.0)),
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.6),
-                      blurRadius: 8,
-                      offset: const Offset(4, 4),
+                    const Divider(
+                      height: 1,
                     ),
-                  ],
-                ),
-                child: Material(
-                  color: Colors.transparent,
-                  child: InkWell(
-                    borderRadius: const BorderRadius.all(Radius.circular(24.0)),
-                    highlightColor: Colors.transparent,
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Center(
-                      child: Text(
-                        'Apply',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            fontSize: 18,
-                            color: Colors.white),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 16, right: 16, bottom: 100, top: 16),
+                      child: Container(
+                        height: 48,
+                        decoration: BoxDecoration(
+                          color: HotelAppTheme.buildLightTheme().primaryColor,
+                          borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.6),
+                              blurRadius: 8,
+                              offset: const Offset(4, 4),
+                            ),
+                          ],
+                        ),
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            borderRadius: const BorderRadius.all(Radius.circular(24.0)),
+                            highlightColor: Colors.transparent,
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Center(
+                              child: Text(
+                                'Apply',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
+                    )
+                  ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -386,7 +386,7 @@ class _FiltersScreenState extends State<FiltersScreen> {
           padding:
           const EdgeInsets.only(left: 16, right: 16, top: 16, bottom: 8),
           child: Text(
-            'Popular filters',
+            'Morning Routine',
             textAlign: TextAlign.left,
             style: TextStyle(
                 color: Colors.grey,
